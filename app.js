4ca40115717f7310d9ab48ba12af589637931008
Ahmed Dillo <142218167+Ahmeddillo@users.noreply.github.com>
2025-08-19 23:56:06 +0300

    app.js

--- app.js
+++ app.js
@@ -5,10 +5,11 @@
   document.getElementById("contact-form").addEventListener("submit", function(e) {
     e.preventDefault();
 
-    emailjs.sendForm(i3hR-yvGVlEUoApUA, template_sibysnn, this)
+    emailjs.sendForm(service_fwzzzje, template_sibysnn, this)
       .then(function(response) {
         alert("Message sent successfully!");
       }, function(error) {
         alert("Failed to send message: " + error.text);
       });
+
   });
